ddcfcfe7c68011f929363291718b3a8d2193715b
curious_odd_man <vlad.varslavans@gmail.com>
2021-05-09 17:54:46 +0300

    #1. Trying to understand how to get output from gradle

--- src/main/java/com/github/curiousoddman/plugins/idea/tee/TeeExecutionListener.java
+++ src/main/java/com/github/curiousoddman/plugins/idea/tee/TeeExecutionListener.java
@@ -4,20 +4,13 @@ import com.intellij.execution.ExecutionListener;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public class TeeExecutionListener implements ExecutionListener {
     private static final Logger log = Logger.getInstance(TeeExecutionListener.class);
 
-    private Project aProject;
-
-    public TeeExecutionListener(Project project) {
-        aProject = project;
-    }
-
     @Override
     public void processStarted(@NotNull String executorId, @NotNull ExecutionEnvironment env, @NotNull ProcessHandler handler) {
-        handler.addProcessListener(new TeeProcessOutputSaver(aProject));
+        handler.addProcessListener(new TeeProcessOutputSaver(env.getProject()));
     }
 }
